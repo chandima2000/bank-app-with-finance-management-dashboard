391ed5957eff41a3e7744f4c30da4c828a9bf490
chandima2000 <hychandima2000@gmail.com>
2024-07-30 22:56:38 +0530

    Implement Header section of Home UI

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,22 @@
+import Header from "@/components/Header";
 
 
 export default function Home() {
+
+  const loggedIn = {firstName : "Chandima"}
   return (
-    <div>
-      Home
-    </div>
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <Header
+            type="greeting"
+            title="welcome"
+            user= {loggedIn?.firstName || "guest"}
+            subtext="Access and Manage your accounts and Transactions Efficiently."
+          />
+        </header>
+      </div>
+    </section>
+
   )
 }
