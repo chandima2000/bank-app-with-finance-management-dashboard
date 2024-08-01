80a5edf4035f9a5cb5e0c42b48699b61d2f7b9c0
chandima2000 <hychandima2000@gmail.com>
2024-08-01 19:51:08 +0530

    Add BankCard component to RightSideBar Component and add styles

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,29 +1,44 @@
 import Header from "@/components/Header";
+import RightSideBar from "@/components/RightSideBar";
 import TotalBalanceBox from "@/components/TotalBalanceBox";
 
 
 export default function Home() {
 
-  const loggedIn = {firstName : "Chandima"}
+  const loggedIn = {
+    firstName: "Chandima", 
+    lastName:"Maduwantha",
+    email:"hychandima2000@gmail.com"
+   }
   return (
-    <section className="home">
-      <div className="home-content">
-        <header className="home-header">
+    <section className="no-scrollbar flex flex-row w-full max-xl:max-h-screen max-xl:overflow-y-scroll">
+      <div className="no-scrollbar flex w-full flex-1 flex-col gap-8 px-5 sm:px-8 py-7 lg:py-12 xl:max-h-screen xl:overflow-y-scroll">
+        <header className="flex flex-col justify-between gap-8">
           <Header
             type="greeting"
             title="welcome"
-            user= {loggedIn?.firstName || "guest"}
+            user={loggedIn?.firstName || "guest"}
             subtext="Access and Manage your accounts and Transactions Efficiently."
           />
 
-          <TotalBalanceBox 
-            accounts = {[]}
-            totalBanks = {3}
-            totalCurrentBalance = {425868}
-            />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={3}
+            totalCurrentBalance={425868}
+          />
 
         </header>
+
+        {/* Recent Transaction */}
       </div>
+
+      {/* Right Sidebar Component */}
+      <RightSideBar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{currentBalance: 423.50},{currentBalance:500.00}]}
+      />
+
     </section>
 
   )
--- components/RightSideBar.tsx
+++ components/RightSideBar.tsx
@@ -1,79 +1,92 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react'
+import BankCard from './BankCard';
 
 
 declare interface RightSidebarProps {
     user: User;
     transactions: Transaction[];
     banks: Bank[] & Account[];
-  }
-  
+}
+
 
 export default function RightSideBar({
-    user,transactions,banks
-} : RightSidebarProps
+    user, transactions, banks
+}: RightSidebarProps
 ) {
-  return (
-    <div className='no-scrollbar hidden h-screen max-h-screen flex-col border-l border-gray-200 xl:flex w-[355px] xl:overflow-y-scroll !important'>
-        {/* Account Details */}
-      <section className='flex flex-col '>
-        
-        <div className="flex flex-row pl-6  max-xl:justify-center gap-4 items-center">
-            <div className="flex-center mt-10 size-14 rounded-full bg-gray-700 border-8 p-2 shadow-profile">
-                <span className='text-xl font-bold text-white '>
-                    {user.firstName[0]}
-                </span>
-            </div>
+    return (
+        <div className='no-scrollbar hidden h-screen max-h-screen flex-col border-l border-gray-200 xl:flex w-[355px] xl:overflow-y-scroll !important'>
+            {/* Account Details */}
+            <section className='flex flex-col '>
 
-            <div className="flex flex-col mt-10">
-                <h1 className='text-14 font-semibold text-gray-900'>
-                    {user.firstName} {user.lastName}
-                </h1>
-                <p className="text-16 font-normal text-gray-600">
-                    {user.email}
-                </p>
-            </div>
-        </div>
+                <div className="flex flex-row pl-6  max-xl:justify-center gap-4 items-center">
+                    <div className="flex-center mt-10 size-14 rounded-full bg-gray-700 border-8 p-2 shadow-profile">
+                        <span className='text-xl font-bold text-white '>
+                            {user.firstName[0]}
+                        </span>
+                    </div>
 
-        {/* Image */}
-        <div className="bg-cover bg-no-repeat mt-1">
-            <Image
-                src={'/icons/finance.svg'}
-                alt='background'
-                width={260}
-                height={100}
-            />
-        </div>
+                    <div className="flex flex-col mt-10">
+                        <h1 className='text-14 font-semibold text-gray-900'>
+                            {user.firstName} {user.lastName}
+                        </h1>
+                        <p className="text-16 font-normal text-gray-600">
+                            {user.email}
+                        </p>
+                    </div>
+                </div>
 
-      </section>
+                {/* Image */}
+                <div className="bg-cover bg-no-repeat mt-1">
+                    <Image
+                        src={'/icons/finance.svg'}
+                        alt='background'
+                        width={260}
+                        height={100}
+                    />
+                </div>
 
-      {/* All Bank Details */}
-      <section className='flex flex-col justify-between gap-8 px-6 py-8'>
-        <div className="flex w-full justify-between">
-            <h2 className='text-18 font-semibold text-gray-900'>My Banks</h2>
-            <Link href="/" className='flex gap-2'>
-                <Image 
-                    src='/icons/plus.svg'
-                    alt='add-btn'
-                    width={20}
-                    height={20}
-                />
-                <h2 className='text-14 font-semibold text-gray-600'>add bank</h2>
-            </Link>
-        </div>
+            </section>
 
-        {banks?.length > 0 && (
-            <div className='relative flex flex-1 flex-col items-center justify-center gap-5'>
-                <div className="relative z-10">Bank Card 1</div>
-                {banks[1] && (
-                    <div className="absolute right-0 top-8 z-0 w-[90%]">
-                        Bank Card 2
+            {/* All Bank Details */}
+            <section className='flex flex-col justify-between gap-8 px-6 py-8'>
+                <div className="flex w-full justify-between">
+                    <h2 className='text-18 font-semibold text-gray-900'>My Banks</h2>
+                    <Link href="/" className='flex gap-2'>
+                        <Image
+                            src='/icons/plus.svg'
+                            alt='add-btn'
+                            width={20}
+                            height={20}
+                        />
+                        <h2 className='text-14 font-semibold text-gray-600'>add bank</h2>
+                    </Link>
+                </div>
+
+                {banks?.length > 0 && (
+                    <div className='relative flex flex-1 flex-col items-center justify-center gap-5'>
+                        <div className="relative z-10">
+                            <BankCard
+                                key={banks[0].$id}
+                                userName={`${user.firstName} ${user.lastName}`}
+                                account={banks[0]}
+                                showBalance={false}
+                            />
+                        </div>
+                        {banks[1] && (
+                            <div className="absolute right-0 top-8 z-0 w-[90%]">
+                                <BankCard
+                                    key={banks[1].$id}
+                                    userName={`${user.firstName} ${user.lastName}`}
+                                    account={banks[1]}
+                                    showBalance={false}
+                                />
+                            </div>
+                        )}
                     </div>
                 )}
-            </div>
-        )}
-      </section>
-    </div>
-  )
+            </section>
+        </div>
+    )
 }
