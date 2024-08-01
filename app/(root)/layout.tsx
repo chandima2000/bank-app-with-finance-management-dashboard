94fa788d9dc1d1629ed6b1c8fc925986a96afa0c
chandima2000 <hychandima2000@gmail.com>
2024-08-01 10:18:12 +0530

    create Layout for SideBar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,19 @@
+import SideBar from "@/components/SideBar";
+import Image from "next/image";
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
+  const loggedIn = { firstName: "Chandima", lastName:"Maduwantha"};
+
   return (
-    <main>
-        SIDEBAR
+    <main className="flex h-screen w-full font-inter">
+        <SideBar user={loggedIn}/>
+
+        
         {children}
     </main>
   );
